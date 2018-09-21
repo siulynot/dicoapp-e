091bcafa6f268d45a44b6021ca33e0bd66c39122
Hoang Nam (particle4dev) <particle4dev@gmail.com>
2018-09-21 15:07:15 +0700

    buypage: improve unit test

--- app/containers/BuyPage/__tests__/reducer.spec.js
+++ app/containers/BuyPage/__tests__/reducer.spec.js
@@ -4,8 +4,8 @@ import {
   loadPrices,
   loadBuyCoin,
   loadBuyCoinError,
-  loadRecentSwapsDataFromWebsocket
-  // loadRecentSwapsCoin
+  loadRecentSwapsDataFromWebsocket,
+  loadRecentSwapsCoin
 } from '../actions';
 import {
   WEBSOCKET_STATE_ONE,
@@ -14,7 +14,14 @@ import {
   WEBSOCKET_STATE_FOUR,
   WEBSOCKET_STATE_FIVE,
   WEBSOCKET_STATE_SIX,
-  WEBSOCKET_STATE_SEVEN
+  WEBSOCKET_STATE_SEVEN,
+  SWAP_STATE_ZERO,
+  SWAP_STATE_ONE,
+  SWAP_STATE_TWO,
+  SWAP_STATE_THREE,
+  SWAP_STATE_FOUR,
+  SWAP_STATE_FIVE
+  // LOAD_SWAP_SUCCESS
 } from './fake-data';
 
 describe('containers/BuyPage/reducers/initial', () => {
@@ -217,6 +224,111 @@ describe('containers/BuyPage/reducers/loadRecentSwapsDataFromWebsocket', () => {
 
 describe('containers/BuyPage/reducers/loadRecentSwapsCoin', () => {
   it('should handle the loadRecentSwapsCoin action correctly', () => {
-    expect(1).toEqual(2);
+    const { uuid } = SWAP_STATE_ZERO;
+    const store = initialState
+      .setIn(['swaps', 'loading'], true)
+      .setIn(['swaps', 'list'], fromJS([SWAP_STATE_ZERO.uuid]))
+      .setIn(
+        ['swaps', 'entities'],
+        fromJS({
+          [SWAP_STATE_ZERO.uuid]: {
+            id: SWAP_STATE_ZERO.tradeid,
+            uuid: SWAP_STATE_ZERO.uuid,
+            requestid: SWAP_STATE_ZERO.requestid,
+            quoteid: SWAP_STATE_ZERO.quoteid,
+            expiration: SWAP_STATE_ZERO.expiration,
+            bob: SWAP_STATE_ZERO.bob,
+            alice: SWAP_STATE_ZERO.alice,
+            bobamount: SWAP_STATE_ZERO.basevalue,
+            aliceamount: SWAP_STATE_ZERO.relvalue,
+            sentflags: [],
+            status: 'pending'
+          }
+        })
+      );
+    let entities = store.getIn(['swaps', 'entities']);
+    let entity = entities.get(uuid);
+    entity = entity
+      .set('sentflags', fromJS(SWAP_STATE_ONE.sentflags))
+      .set('expiration', SWAP_STATE_ONE.expiration)
+      .set('requestid', SWAP_STATE_ONE.requestid)
+      .set('quoteid', SWAP_STATE_ONE.quoteid)
+      .set('bobamount', SWAP_STATE_ONE.srcamount)
+      .set('aliceamount', SWAP_STATE_ONE.destamount);
+    let expectedResult = store.setIn(
+      ['swaps', 'entities'],
+      entities.set(uuid, entity)
+    );
+    expect(buyReducer(store, loadRecentSwapsCoin(SWAP_STATE_ONE))).toEqual(
+      expectedResult
+    );
+
+    entities = store.getIn(['swaps', 'entities']);
+    entity = entities.get(uuid);
+    entity = entity
+      .set('sentflags', fromJS(SWAP_STATE_TWO.sentflags))
+      .set('expiration', SWAP_STATE_TWO.expiration)
+      .set('requestid', SWAP_STATE_TWO.requestid)
+      .set('quoteid', SWAP_STATE_TWO.quoteid)
+      .set('bobamount', SWAP_STATE_TWO.srcamount)
+      .set('aliceamount', SWAP_STATE_TWO.destamount);
+    expectedResult = store.setIn(
+      ['swaps', 'entities'],
+      entities.set(uuid, entity)
+    );
+    expect(buyReducer(store, loadRecentSwapsCoin(SWAP_STATE_TWO))).toEqual(
+      expectedResult
+    );
+
+    entities = store.getIn(['swaps', 'entities']);
+    entity = entities.get(uuid);
+    entity = entity
+      .set('sentflags', fromJS(SWAP_STATE_THREE.sentflags))
+      .set('expiration', SWAP_STATE_THREE.expiration)
+      .set('requestid', SWAP_STATE_THREE.requestid)
+      .set('quoteid', SWAP_STATE_THREE.quoteid)
+      .set('bobamount', SWAP_STATE_THREE.srcamount)
+      .set('aliceamount', SWAP_STATE_THREE.destamount);
+    expectedResult = store.setIn(
+      ['swaps', 'entities'],
+      entities.set(uuid, entity)
+    );
+    expect(buyReducer(store, loadRecentSwapsCoin(SWAP_STATE_THREE))).toEqual(
+      expectedResult
+    );
+
+    entities = store.getIn(['swaps', 'entities']);
+    entity = entities.get(uuid);
+    entity = entity
+      .set('sentflags', fromJS(SWAP_STATE_FOUR.sentflags))
+      .set('expiration', SWAP_STATE_FOUR.expiration)
+      .set('requestid', SWAP_STATE_FOUR.requestid)
+      .set('quoteid', SWAP_STATE_FOUR.quoteid)
+      .set('bobamount', SWAP_STATE_FOUR.srcamount)
+      .set('aliceamount', SWAP_STATE_FOUR.destamount);
+    expectedResult = store.setIn(
+      ['swaps', 'entities'],
+      entities.set(uuid, entity)
+    );
+    expect(buyReducer(store, loadRecentSwapsCoin(SWAP_STATE_FOUR))).toEqual(
+      expectedResult
+    );
+
+    entities = store.getIn(['swaps', 'entities']);
+    entity = entities.get(uuid);
+    entity = entity
+      .set('sentflags', fromJS(SWAP_STATE_FIVE.sentflags))
+      .set('expiration', SWAP_STATE_FIVE.expiration)
+      .set('requestid', SWAP_STATE_FIVE.requestid)
+      .set('quoteid', SWAP_STATE_FIVE.quoteid)
+      .set('bobamount', SWAP_STATE_FIVE.srcamount)
+      .set('aliceamount', SWAP_STATE_FIVE.destamount)
+      .set('status', 'finished');
+    expectedResult = store
+      .setIn(['swaps', 'entities'], entities.set(uuid, entity))
+      .setIn(['swaps', 'loading'], false);
+    expect(buyReducer(store, loadRecentSwapsCoin(SWAP_STATE_FIVE))).toEqual(
+      expectedResult
+    );
   });
 });
